6aad544468643d599581f8048cdf166ad65f6a57
Ayush Rawal <31813648+Oguz-Khagan@users.noreply.github.com>
2018-02-09 21:51:05 +0530

    Added admin login page and some fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
