48f5273b385f29fa4764909c7c377d20129a4ba6
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2018-03-14 23:09:06 +0530

    Added backend dependencies

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
-const webapp=require('./server/webapp');
+const express = require('express');
+const bodyparser = require('body-parser');
+const global_routes = require('./server/global/global_routes');
+const mongoose = require('mongoose');
+const webapp = express();
+
+webapp.Promise = global.Promise;
+webapp.use(bodyparser.json());
+
+// mongoose.connect('mongodb://localhost:27017/iiit');
+
+webapp.use(express.static(__dirname + '/dist'))
+
+global_routes(webapp);
+
+webapp.use((err,req,res,next)=>{
+    console.log(res.status(422).send({error:err.message}));
+});
 
 webapp.listen(process.env.PORT || 8080, ()=>{
     console.log(`Server running on ${process.env.PORT || 8080}`);
