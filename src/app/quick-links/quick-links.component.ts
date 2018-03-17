8f56e0fc0a483f76b30e072718a2654e7e401e4c
Shubhcr7 <shubhamsharmacr7@gmail.com>
2018-03-18 02:02:45 +0530

    added a search site option inside quick links , go check it out

--- src/app/quick-links/quick-links.component.ts
+++ src/app/quick-links/quick-links.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import { Router } from '@angular/router';
+
+const states = ['About-us','Contact-Us','Students','Placement','Fee Structure','Admissions','Library','Curriculum','Scholarships'];
 
 @Component({
   selector: 'quick-links',
@@ -7,7 +14,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class QuickLinksComponent {
-  constructor() { }
+  constructor(public router:Router) { }
 
   public content = [
       {
@@ -21,5 +28,52 @@ export class QuickLinksComponent {
         "link": "/about"
       }
     ]
+    routing(model:string)
+    { model=model.toLowerCase();
+      if(model=="admin")
+      this.router.navigate(['../admin']);
+      
+      else if(model=="about-us")
+      this.router.navigate(['../about']);
+
+      else if(model=="contact-us")
+      this.router.navigate(['contact-us']);
+
+      
+      else if(model=="students")
+      this.router.navigate(['../students/placement']);
+
+      
+      else if(model=="placement")
+      this.router.navigate(['../students/current-batches']);
+
+      
+      else if(model=="fee structure")
+      this.router.navigate(['../institute/fees-registration']);
+
+      
+      else if(model=="admissions")
+      this.router.navigate(['../institute/admissions']);
+
+      
+      else if(model=="library")
+      this.router.navigate(['../academics/library']);
+
+      else if(model=="curriculum")
+      this.router.navigate(['../academics/curriculum']);
+
+      else if(model=="scholarships")
+      this.router.navigate(['../academics/scholarships']);
+
+    }
+  public model: any;
+  
+  search = (text$: Observable<string>) =>
+    text$
+      .debounceTime(200)
+      .distinctUntilChanged()
+      .map(term => term.length < 1 ? []
+        : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10));
+
 
 }
