010842ad22934bc8e6a62e8a46d5e312eef849f6
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2018-03-17 20:17:12 +0530

    Refactored Industry partners and made them static(they aren't gonna change)

--- src/app/industry-partners/industry-partners.component.ts
+++ src/app/industry-partners/industry-partners.component.ts
@@ -1,18 +1,10 @@
-import { Http } from '@angular/http';
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 @Component({
   selector: 'industry-partners',
   templateUrl: './industry-partners.component.html',
   styleUrls: ['./industry-partners.component.css']
 })
-export class IndustryPartnersComponent implements OnInit {
-  res:any[];
-  constructor(http:Http) {
-    http.get("http://jsonplaceholder.typicode.com/users").subscribe(response=>{
-      this.res=response.json();
-    }); //end point for displaying the industry-partners name
-   }
-  ngOnInit() {
-  }
+export class IndustryPartnersComponent {
 
+  constructor() { }
 }
