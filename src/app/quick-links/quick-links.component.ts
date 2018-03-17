9a2523cba57934245fdd5c9f7125f1aa237c5a8e
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2018-03-17 18:12:41 +0530

    Quick Links finished

--- src/app/quick-links/quick-links.component.ts
+++ src/app/quick-links/quick-links.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import {Http} from '@angular/http'
 
 @Component({
   selector: 'quick-links',
   templateUrl: './quick-links.component.html',
   styleUrls: ['./quick-links.component.css']
 })
-export class QuickLinksComponent implements OnInit {
-links;
-  constructor(http:Http) {
-    http.get('http://jsonplaceholder.typicode.com/users').subscribe(response=>{
-      this.links=response.json();
-    });
-   }
 
-  ngOnInit() {
-  }
+export class QuickLinksComponent {
+  constructor() { }
+
+  public content = [
+      {
+        "name": "Vacancies",
+        "link": "/vacancies"
+      }, {
+        "name": "Tenders",
+        "link": "/tenders"
+      }, {
+        "name": "About us",
+        "link": "/about"
+      }
+    ]
 
 }
