11fbe0b4f952920d09325de8f7c7225ad9aecf76
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2018-03-18 07:13:29 +0530

    Refactored permanent faculty

--- src/app/permanent-faculty/permanent-faculty.component.ts
+++ src/app/permanent-faculty/permanent-faculty.component.ts
@@ -5,14 +5,48 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './permanent-faculty.component.html',
   styleUrls: ['./permanent-faculty.component.css']
 })
-export class PermanentFacultyComponent implements OnInit {
-  res:any[];
-  constructor(http:Http) {
-    http.get("http://jsonplaceholder.typicode.com/users").subscribe(response=>{
-      this.res=response.json();
-    }); //end point for displaying the faculties
-   }
-  ngOnInit() {
-  }
+export class PermanentFacultyComponent {
+
+  constructor() { }
+
+  content = [
+    {
+      name: "Dr. Pooja Jain",
+      department: "CSE",
+      contact: "9549650438"
+    }, {
+      name: "Dr. Tapan Kumar Jain",
+      department: "ECE",
+      contact: "9549650206"
+    }, {
+      name: "Dr. Smita Naval",
+      department: "CSE",
+      contact: "9549651074"
+    }, {
+      name: "Dr. Amit Kumar",
+      department: "CSE",
+      contact: "9549655278"
+    }, {
+      name: "Dr. Parikshit Singh",
+      department: "ECE",
+      contact: "9549659156"
+    }, {
+      name: "Dr. Neeraj Rao",
+      department: "ECE",
+      contact: "9549651197"
+    }, {
+      name: "Dr. Kalpana Naidu",
+      department: "ECE",
+      contact: "9549650388"
+    }, {
+      name: "Dr. Sangeeta Singh",
+      department: "ECE",
+      contact: "9549651479"
+      }, {
+      name: "Dr. Vinita Tiwari",
+      department: "ECE",
+      contact: "9549654678"
+    }
+  ]
 
 }
