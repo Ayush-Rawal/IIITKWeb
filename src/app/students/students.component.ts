4af15e09b23cb46dbbf63ae0c7468a1bac149d5f
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2018-03-19 20:09:58 +0530

    Added pdf links to scholarship

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,18 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import {Http} from '@angular/http';
+import { Component } from '@angular/core';
 @Component({
   selector: 'students',
   templateUrl: './students.component.html',
   styleUrls: ['./students.component.css']
 })
-export class StudentsComponent implements OnInit {
-  res:any[];
-  constructor(http:Http) {
-    http.get("http://jsonplaceholder.typicode.com/photos").subscribe(response=>{
-      this.res=response.json();
-    }); //end point for displaying the students pdf link
-   }
-  ngOnInit() {
-  }
+export class StudentsComponent {
+
+  constructor() { }
+
+  public content = [
+    {
+      batch: "CSE, 2013",
+      link: "/pdf/Students/2013-CSE.pdf"
+    }, {
+      batch: "CSE, 2014",
+      link: "/pdf/Students/2014-CSE.pdf"
+    }, {
+      batch: "CSE, 2015",
+      link: "/pdf/Students/2015-CSE.pdf"
+    }, {
+      batch: "ECE, 2015",
+      link: "/pdf/Students/2015-ECE.pdf"
+    }, {
+      batch: "CSE, 2016",
+      link: "/pdf/Students/2016-CSE.pdf"
+    }, {
+      batch: "ECE, 2016",
+      link: "/pdf/Students/2016-ECE.pdf"
+    }, {
+      batch: "CSE, 2017",
+      link: "/pdf/Students/2017-CSE.pdf"
+    }, {
+      batch: "ECE, 2017",
+      link: "/pdf/Students/2017-ECE.pdf"
+    }
+  ]
 
 }
