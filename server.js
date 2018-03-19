dafceada7dcf896a493f44a70f8640a9d2acb1d1
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2018-03-19 18:40:05 +0530

    added morgan

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 const express = require('express');
-const bodyparser = require('body-parser');
-const global_routes = require('./server/global/global_routes');
-const mongoose = require('mongoose');
 const morgan = require('morgan')
+const fs = require('fs')
 const app = express();
 
-app.Promise = global.Promise;
-app.use(bodyparser.json());
-
-// mongoose.connect('mongodb://localhost:27017/iiit');
+const logStream = fs.createWriteStream(`${__dirname}/server/.log`, {flags: 'a'})
+app.use(morgan('dev', {stream: logStream}))
 
 app.use(express.static(__dirname + '/dist'))
-
-global_routes(app);
-
-app.use((err,req,res,next)=>{
-    console.log(res.status(422).send({error:err.message}));
-});
 
 app.listen(process.env.PORT || 8080, ()=>{
     console.log(`Server running on ${process.env.PORT || 8080}`);
