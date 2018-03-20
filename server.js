1500072660829d1d36b16ffd8476d92ce56c5d69
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2018-03-20 10:44:57 +0530

    Fixed direct URL GET requests not working

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get('/pdf/:folder/:file', (req,res) => {
 
 app.use(express.static(__dirname + '/dist'))
 
+app.get('*', (req,res) => {
+    res.status(200).sendFile(`${__dirname}/dist/index.html`)
+})
+
 app.listen(process.env.PORT || 8080, ()=>{
     console.log(`Server running on ${process.env.PORT || 8080}`);
 });
