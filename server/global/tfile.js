288ca0bc16848a1e6681314c0d48e25c2612953d
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2018-03-20 13:10:24 +0530

    Removed old models and controllers

--- server/global/tfile.js
+++
@@ -1,6 +0,0 @@
-var data = [{"name":"placeHolder","section":"right"},{"name":"Overview","section":"left"},{"name":"ByFunction","section":"left"},{"name":"Time","section":"left"},{"name":"allFit","section":"left"},{"name":"allbMatches","section":"left"},{"name":"allOffers","section":"left"},{"name":"allInterests","section":"left"},{"name":"allResponses","section":"left"},{"name":"divChanged","section":"right"}];
-
-var val = "allInterests"
-var index = data.findIndex(function(item, i){
-  return item.name === val
-});
--- server/models/global_model.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
-const GlobalSchema=new Schema({
-    // about,mission,vision  Schema
-    name:String,
-    content:String,
-});
-const Global=mongoose.model('global',GlobalSchema);
-module.exports=Global;
