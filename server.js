563c8945c979b0e30056e70a13f8ffe2b4259f1e
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2018-03-20 13:47:17 +0530

    Added cors for mlab

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const fs = require('fs')
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express();
 app.use(bodyparser.json());
@@ -11,9 +12,16 @@ app.use(bodyparser.urlencoded({extended: true}))
 const logStream = fs.createWriteStream(`${__dirname}/server/.log`, {flags: 'a'})
 app.use(morgan('dev', {stream: logStream}))
 
+app.use(cors({
+    origin: 'https://api.mlab.com'
+}))
 const dbURL = process.env.dbURL || 'mongodb://AyushRawal:iiitkota@ds119059.mlab.com:19059/iiitkweb'
 mongoose.connect(dbURL)
 const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('DB Connected')
+});
 
 app.get('/pdf/:folder/:file', (req,res) => {
     res.sendFile(`${__dirname}/src/assets/pdf/${req.params.folder}/${req.params.file}`)
