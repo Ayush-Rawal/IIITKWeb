c2f2c296a22203cb5cd59145565de25548a299df
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2018-03-20 11:49:35 +0530

    added subschema for representativs, minutes, director, prev director, coordinator, prev coordinator and links

---
+++ server/subschemas.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+const Schema =  mongoose.Schema
+
+module.exports = {
+    representative: new Schema({
+        name: String,
+        designation: String
+    }),
+    
+    minute: new Schema({
+        title: String,
+        link: String,
+        timestamp: String
+    }),
+    
+    director: new Schema({
+        name: String,
+        office: String,
+        email: String,
+        website: String,
+        tenure: String
+    }),
+    
+    prevDirector: new Schema({
+        name: String,
+        office: String,
+        email: String,
+        tenure: String
+    }),
+    
+    coordinator: new Schema({
+        name: String,
+        email: String,
+        office: String,
+        education: String,
+        phone: String,
+        website: String,
+        tenure: String
+    }),
+    
+    prevCoordinator: new Schema({
+        name: String,
+        tenure: String
+    }),
+
+    link: new Schema({
+        name: String,
+        link: String
+    }),
+}    
