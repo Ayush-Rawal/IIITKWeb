c959b1603fde6b893cfdaf056eea79ed4120e7ba
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2018-03-22 13:25:43 +0530

    Changed slide time to 5 sec

--- src/app/images-slide/images-slide.component.ts
+++ src/app/images-slide/images-slide.component.ts
@@ -9,7 +9,7 @@ export class ImagesSlideComponent  {
 
   constructor(config: NgbCarouselConfig) {
     // customize default values of carousels used by this component tree
-    config.interval = 1000;
+    config.interval = 5000;
     config.wrap = true;
     config.keyboard = true;
   }
