d077b3410822089c5cf357d82c7d1b57aa027067
Shubhcr7 <shubhamsharmacr7@gmail.com>
2018-03-30 20:01:27 +0530

    library , navbar check

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -1,3 +1,4 @@
+import { ApiService } from './../services/api.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,62 +8,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibraryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api:ApiService) { }
 
-  public content = {
-    library: [
-      {
-        name: "Digital Library",
-        links: [
-          {
-            name: "RemoteXs",
-            link: "http://mnitj.remotexs.in/"
-          }
-        ],
-        info: "For anytime anywhere access of IP-based subscribed online resources (databases, publications in journals and conferences), we are using RemoteXs solution.\
-        Note: Use your LDAP ID and password to login. Do not share your credentials. Access is being monitored. "
-      },
-      {
-        name: "NPTEL",
-        links: [
-          {
-            name: "CSE Courses at NPTEL",
-            link: "http://nptel.ac.in/course.php?disciplineId=106"
-          }, {
-            name: "ECE Courses at NPTEL",
-            link: "http://nptel.ac.in/course.php?disciplineId=117"
-          }
-        ],
-        info: "You are encouraged to leverage following NPTEL resources:"
-      },
-      {
-        name: "SWAYAM",
-        links: [
-          {
-            name: "SWAYAM",
-            link: "https://swayam.gov.in/About"
-          }, {
-            name: "SWAYAM portal for undergrads",
-            link: "https://swayam.gov.in/Undergraduate"
-          }
-        ],
-        info: "SWAYAM is an initiative by GoI to make some of the best learning resources accessible to all. "
-      },
-      {
-        name: "National Digital Library",
-        links: [
-          {
-            name: "National Digital Library",
-            link: "https://ndl.iitkgp.ac.in/"
-          }
-        ],
-        info: " "
+  // public content = {
+  //   library: [
+  //     {
+  //       name: "Digital Library",
+  //       links: [
+  //         {
+  //           name: "RemoteXs",
+  //           link: "http://mnitj.remotexs.in/"
+  //         }
+  //       ],
+  //       info: "For anytime anywhere access of IP-based subscribed online resources (databases, publications in journals and conferences), we are using RemoteXs solution.\
+  //       Note: Use your LDAP ID and password to login. Do not share your credentials. Access is being monitored. "
+  //     },
+  //     {
+  //       name: "NPTEL",
+  //       links: [
+  //         {
+  //           name: "CSE Courses at NPTEL",
+  //           link: "http://nptel.ac.in/course.php?disciplineId=106"
+  //         }, {
+  //           name: "ECE Courses at NPTEL",
+  //           link: "http://nptel.ac.in/course.php?disciplineId=117"
+  //         }
+  //       ],
+  //       info: "You are encouraged to leverage following NPTEL resources:"
+  //     },
+  //     {
+  //       name: "SWAYAM",
+  //       links: [
+  //         {
+  //           name: "SWAYAM",
+  //           link: "https://swayam.gov.in/About"
+  //         }, {
+  //           name: "SWAYAM portal for undergrads",
+  //           link: "https://swayam.gov.in/Undergraduate"
+  //         }
+  //       ],
+  //       info: "SWAYAM is an initiative by GoI to make some of the best learning resources accessible to all. "
+  //     },
+  //     {
+  //       name: "National Digital Library",
+  //       links: [
+  //         {
+  //           name: "National Digital Library",
+  //           link: "https://ndl.iitkgp.ac.in/"
+  //         }
+  //       ],
+  //       info: " "
+  //     }
+  //   ]
+  // }
+
+
+  public content;
+  ngOnInit()
+  {
+    this.api.getData('/api/library').subscribe(
+      res=>{
+       this.content=res.json(); 
       }
-    ]
+    )
   }
-
-  ngOnInit() {
-    window.scrollTo(0,0);
-  }
-
 }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,3 +1,4 @@
+import { ApiService } from './../services/api.service';
 import { Component, Input } from '@angular/core';
 
 @Component({
@@ -7,98 +8,109 @@ import { Component, Input } from '@angular/core';
 })
 export class NavBarComponent {
 
-  constructor() { }
+  constructor(private api:ApiService) { }
 
-  content = {
-    "links": [
-      {
-        name: "Institute",
-        link: "",
-        sublinks: [
-          {
-            name: "Executive Council",
-            link: "/institute/executive-council"
-          }, {
-            name: "Governing Body",
-            link: "/institute/governing-body"
-          }, {
-            name: "Senate",
-            link: "/institute/senate"
-          }, {
-            name: "Director",
-            link: "/institute/director"
-          }, {
-            name: "Coordinator",
-            link: "/institute/coordinator"
-          }, {
-            name: "Industry Partners",
-            link: "/institute/industry-partners"
-          }, {
-            name: "Admissions",
-            link: "/institute/admissions"
-          }, {
-            name: "Fees and Registrations",
-            link: "/institute/fees-registration"
-          }
-        ]
-      }, {
-        name: "Academics",
-        link: "",
-        sublinks: [
-          {
-            name: "Departments",
-            link: "/academics/departments"
-          }, {
-            name: "Time Table",
-            link: "/academics/timetable"
-          }, {
-            name: "Curriculum",
-            link: "/academics/curriculum"
-          }, {
-            name: "Scholarships",
-            link: "/academics/scholarships"
-          }, {
-            name: "Library",
-            link: "/academics/library"
-          }, {
-            name: "Events",
-            link: "/academics/institute-events"
-          }
-        ]
-      }, {
-        "name": "Faculty",
-        "link": "",
-        "sublinks": [
-          {
-            "name": "Contract Faculty",
-            "link": "/faculty/contract"
-          },
-          {
-            "name": "Guest Faculty",
-            "link": "/faculty/guest"
-          },
-          {
-            "name": "Non-Teaching-Staff",
-            "link": "/faculty/non-teaching-staff"
-          }
-        ]
-      }, {
-          "name": "Students",
-          "link": "",
-          "sublinks": [
-            {
-              name: "Current Batches",
-              link: "/students/current-batches"
-            }, {
-              name: "Placements",
-              link: "/students/placement"
-            }
-          ]
-      }, {
-        "name": "Contact Us",
-        "link": "/contact-us",
-        "sublinks": []
+  // content = {
+  //   "links": [
+  //     {
+  //       name: "Institute",
+  //       link: "",
+  //       sublinks: [
+  //         {
+  //           name: "Executive Council",
+  //           link: "/institute/executive-council"
+  //         }, {
+  //           name: "Governing Body",
+  //           link: "/institute/governing-body"
+  //         }, {
+  //           name: "Senate",
+  //           link: "/institute/senate"
+  //         }, {
+  //           name: "Director",
+  //           link: "/institute/director"
+  //         }, {
+  //           name: "Coordinator",
+  //           link: "/institute/coordinator"
+  //         }, {
+  //           name: "Industry Partners",
+  //           link: "/institute/industry-partners"
+  //         }, {
+  //           name: "Admissions",
+  //           link: "/institute/admissions"
+  //         }, {
+  //           name: "Fees and Registrations",
+  //           link: "/institute/fees-registration"
+  //         }
+  //       ]
+  //     }, {
+  //       name: "Academics",
+  //       link: "",
+  //       sublinks: [
+  //         {
+  //           name: "Departments",
+  //           link: "/academics/departments"
+  //         }, {
+  //           name: "Time Table",
+  //           link: "/academics/timetable"
+  //         }, {
+  //           name: "Curriculum",
+  //           link: "/academics/curriculum"
+  //         }, {
+  //           name: "Scholarships",
+  //           link: "/academics/scholarships"
+  //         }, {
+  //           name: "Library",
+  //           link: "/academics/library"
+  //         }, {
+  //           name: "Events",
+  //           link: "/academics/institute-events"
+  //         }
+  //       ]
+  //     }, {
+  //       "name": "Faculty",
+  //       "link": "",
+  //       "sublinks": [
+  //         {
+  //           "name": "Contract Faculty",
+  //           "link": "/faculty/contract"
+  //         },
+  //         {
+  //           "name": "Guest Faculty",
+  //           "link": "/faculty/guest"
+  //         },
+  //         {
+  //           "name": "Non-Teaching-Staff",
+  //           "link": "/faculty/non-teaching-staff"
+  //         }
+  //       ]
+  //     }, {
+  //         "name": "Students",
+  //         "link": "",
+  //         "sublinks": [
+  //           {
+  //             name: "Current Batches",
+  //             link: "/students/current-batches"
+  //           }, {
+  //             name: "Placements",
+  //             link: "/students/placement"
+  //           }
+  //         ]
+  //     }, {
+  //       "name": "Contact Us",
+  //       "link": "/contact-us",
+  //       "sublinks": []
+  //     }
+  //   ]
+  // }
+
+
+  public content;
+  ngOnInit()
+  {
+    this.api.getData('/api/navbar').subscribe(
+      res=>{
+       this.content=res.json(); 
       }
-    ]
+    )
   }
 }
