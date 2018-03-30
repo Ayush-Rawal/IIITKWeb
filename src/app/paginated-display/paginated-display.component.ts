603c573cc3dc17e997ed96c3328612b882c4e5c7
Shubhcr7 <shubhamsharmacr7@gmail.com>
2018-03-30 20:14:08 +0530

    Paginated display has nginit under /api/news

--- src/app/paginated-display/paginated-display.component.ts
+++ src/app/paginated-display/paginated-display.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ApiService } from './../services/api.service';
+import { Component, OnInit } from '@angular/core';
 import {
   style,
   trigger,
@@ -20,93 +21,94 @@ import {
     ])
   ]
 })
-export class PaginatedDisplayComponent {
+export class PaginatedDisplayComponent implements OnInit {
 
-  constructor() { }
-  public content= {
-    title:"Latest News",
-    btnDisp:"Gimme More!",
-    btnLink:"",
-    items:[
-      {
-        content:"Internships and Placements",
-        link:"../students/placement"
-      },{
-        content:"Institute Scholarships",
-        link:"../academics/scholarships"
-      },{
-        content:"Vidyalaxmi Portal For Education Loan",
-         link:"" //don't think we should make a component for this
-      },{
-        content:"Notice for appointment on deputation basis",
-        link:"../vacancies"
-      },{
-        content:"Advertisement for the 14 Posts of the Director IIIT (PPP) under MHRD",
-        link:"../vacancies"
-      },{
-        content:"Faculty Positions at IIIT-K date extended",
-        link:"../vacancies"
-      },{
-        content:"Orientation Program",
-        link:"" //no need of component for it
-      },{
-        content:"IIIT Kota declared as institute of national importance",
-        link:""
-      },{
-        content:"Guest Faculty",
-        link:"../faculty/guest"
-      },{
-        content:"Content 10",
-        link:""
-      },{
-        content:"Content 11",
-        link:""
-      },{
-        content:"Content 12",
-        link:""
-      },{
-        content:"Content 13",
-        link:""
-      },{
-        content:"Content 14",
-        link:""
-      },{
-        content:"Content 15",
-        link:""
-      },{
-        content:"Content 16",
-        link:""
-      },{
-        content:"Content 17",
-        link:""
-      },{
-        content:"Content 18",
-        link:""
-      },{
-        content:"Content 19",
-        link:""
-      },{
-        content:"Content 20",
-        link:""
-      },{
-        content:"Content 21",
-        link:""
-      },{
-        content:"Content 22",
-        link:""
-      },{
-        content:"Content 23",
-        link:""
-      },{
-        content:"Content 24",
-        link:""
-      },{
-        content:"Content 25",
-        link:""
-      }
-    ]
-  }
+  constructor(private api:ApiService) { }
+  // public content= {
+  //   title:"Latest News",
+  //   btnDisp:"Gimme More!",
+  //   btnLink:"",
+  //   items:[
+  //     {
+  //       content:"Internships and Placements",
+  //       link:"../students/placement"
+  //     },{
+  //       content:"Institute Scholarships",
+  //       link:"../academics/scholarships"
+  //     },{
+  //       content:"Vidyalaxmi Portal For Education Loan",
+  //        link:"" //don't think we should make a component for this
+  //     },{
+  //       content:"Notice for appointment on deputation basis",
+  //       link:"../vacancies"
+  //     },{
+  //       content:"Advertisement for the 14 Posts of the Director IIIT (PPP) under MHRD",
+  //       link:"../vacancies"
+  //     },{
+  //       content:"Faculty Positions at IIIT-K date extended",
+  //       link:"../vacancies"
+  //     },{
+  //       content:"Orientation Program",
+  //       link:"" //no need of component for it
+  //     },{
+  //       content:"IIIT Kota declared as institute of national importance",
+  //       link:""
+  //     },{
+  //       content:"Guest Faculty",
+  //       link:"../faculty/guest"
+  //     },{
+  //       content:"Content 10",
+  //       link:""
+  //     },{
+  //       content:"Content 11",
+  //       link:""
+  //     },{
+  //       content:"Content 12",
+  //       link:""
+  //     },{
+  //       content:"Content 13",
+  //       link:""
+  //     },{
+  //       content:"Content 14",
+  //       link:""
+  //     },{
+  //       content:"Content 15",
+  //       link:""
+  //     },{
+  //       content:"Content 16",
+  //       link:""
+  //     },{
+  //       content:"Content 17",
+  //       link:""
+  //     },{
+  //       content:"Content 18",
+  //       link:""
+  //     },{
+  //       content:"Content 19",
+  //       link:""
+  //     },{
+  //       content:"Content 20",
+  //       link:""
+  //     },{
+  //       content:"Content 21",
+  //       link:""
+  //     },{
+  //       content:"Content 22",
+  //       link:""
+  //     },{
+  //       content:"Content 23",
+  //       link:""
+  //     },{
+  //       content:"Content 24",
+  //       link:""
+  //     },{
+  //       content:"Content 25",
+  //       link:""
+  //     }
+  //   ]
+  // }
   
+  public content;
   public currPage = 1;
   public perPage = 6;
   public maxPage = Math.ceil(this.content.items.length/this.perPage);
@@ -115,6 +117,15 @@ export class PaginatedDisplayComponent {
   changePage = (page) => {
     this.currPage = this.currPage + page;
     this.currList = this.content.items.slice((this.currPage-1) * this.perPage, this.currPage * this.perPage);
+  }
+
+  ngOnInit()
+  {
+    this.api.getData('/api/news').subscribe(
+      res=>{
+       this.content=res.json(); 
+      }
+    )
   }
 
 }
