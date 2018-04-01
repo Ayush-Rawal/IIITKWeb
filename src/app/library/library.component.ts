3780a7422d11114bcbd6d9c03446af5ad26b4d2a
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2018-04-01 14:45:20 +0530

    Made GB and library dynamic

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'library',
@@ -7,62 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibraryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
-  public content = {
-    library: [
-      {
-        name: "Digital Library",
-        links: [
-          {
-            name: "RemoteXs",
-            link: "http://mnitj.remotexs.in/"
-          }
-        ],
-        info: "For anytime anywhere access of IP-based subscribed online resources (databases, publications in journals and conferences), we are using RemoteXs solution.\
-        Note: Use your LDAP ID and password to login. Do not share your credentials. Access is being monitored. "
-      },
-      {
-        name: "NPTEL",
-        links: [
-          {
-            name: "CSE Courses at NPTEL",
-            link: "http://nptel.ac.in/course.php?disciplineId=106"
-          }, {
-            name: "ECE Courses at NPTEL",
-            link: "http://nptel.ac.in/course.php?disciplineId=117"
-          }
-        ],
-        info: "You are encouraged to leverage following NPTEL resources:"
-      },
-      {
-        name: "SWAYAM",
-        links: [
-          {
-            name: "SWAYAM",
-            link: "https://swayam.gov.in/About"
-          }, {
-            name: "SWAYAM portal for undergrads",
-            link: "https://swayam.gov.in/Undergraduate"
-          }
-        ],
-        info: "SWAYAM is an initiative by GoI to make some of the best learning resources accessible to all. "
-      },
-      {
-        name: "National Digital Library",
-        links: [
-          {
-            name: "National Digital Library",
-            link: "https://ndl.iitkgp.ac.in/"
-          }
-        ],
-        info: " "
-      }
-    ]
-  }
+  public content
 
   ngOnInit() {
-    window.scrollTo(0,0);
+    this.api.getData('/api/library')
+    .subscribe((res) => this.content = res.json()[0])
+    window.scrollTo(0, 0)
   }
 
 }
