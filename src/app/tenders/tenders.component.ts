fdfc0c998ab4bcb0c24d2d85482aaa2fa3464819
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2018-04-02 05:10:26 +0530

    Tenders complete(static)

--- src/app/tenders/tenders.component.ts
+++ src/app/tenders/tenders.component.ts
@@ -14,29 +14,25 @@ export class TendersComponent implements OnInit{
       "name": "Limited Tender Inquiry for Supply & Installation of Furniture for Classroom",
       "desc": "IIITK/11/2017-18",
       "link": {
-        "name":"Tender Document",
-        "link":""
-           },
-      "osublink":{
-        "name":"",
-        "link":"",
-      }
+        "name": "Tender Document",
+        "link": "/pdf/Tender/tender-class-furniture.pdf"
+           }
     },
     {
-      "name": "",
-      "desc": "",
+      "name": "The Limited Tender Document for purchase of Books for Library IIIT Kota",
+      "desc": "The List of Books is attached herewith.",
       "link": {
-        "name":"",
-        "link":""
+        "name": "Tender Document",
+        "link": "/pdf/Tender/tender-library.pdf"
            },
-      "osublink":{
-        "name":"",
-        "link":"",
+      "sublink": {
+        "name": "List of Books",
+        "link": "/pdf/Tender/List-of-Book-Bank-Books1.pdf",
       }
     },
   ]
 
-  ngOnInit(){
-  window.scrollTo(0,0);
+  ngOnInit() {
+  window.scrollTo(0, 0)
   }
 }
